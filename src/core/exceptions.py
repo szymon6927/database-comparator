198900a2e9b26e705f645d783e46e173b5e58a91
Szymon Miks <szymon.miks@netguru.com>
2020-01-09 13:43:40 +0100

    [BN-1019]: Added NotExists exception

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -2,3 +2,9 @@ class MissingEnvVariableError(Exception):
     """raise when env variable is not set"""
 
     pass
+
+
+class NotExists(Exception):
+    """raise when object not exist"""
+
+    pass
