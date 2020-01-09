862d2508393bf205367edbff906897e285550706
Szymon Miks <szymon.miks@netguru.com>
2020-01-09 12:05:34 +0100

    [BN-1019]: Added initial version of main application script

---
+++ database_comparator.py
@@ -0,0 +1,11 @@
+from src.mysql_dirver.driver import MySQLDriver
+
+
+def main():
+    print("Welcome in Database Comparator")
+    mysql_driver = MySQLDriver()
+    mysql_driver.run_tests()
+
+
+if __name__ == '__main__':
+    main()
