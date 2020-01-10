6256b327ac007686a977315526e03768aaf57708
Szymon Miks <szymon.miks@netguru.com>
2020-01-10 11:33:51 +0100

    [BN-1019]: Added click as CLI for database_comparator

--- database_comparator.py
+++ database_comparator.py
@@ -1,10 +1,32 @@
-from drivers.mysql.driver import MySQLDriver
+import sys
+
+import click
+
+from src.cli import SUPPORTED_DATABASES_CLI_ARGS
+from src.cli import is_valid_database_cli_arg
+from src.core.factory import MultipleDatabaseTypeFactory
 
 
-def main():
-    print("Welcome in Database Comparator")
-    mysql_driver = MySQLDriver()
-    mysql_driver.run_tests()
+@click.command()
+@click.option('--database', '-db', help='database type which you want to test')
+@click.option('--operations-number', '-opn', default=10, help='number of operations performed on the database')
+def main(database, operations_number):
+    """
+    Database Comparator - Netguru RnD project
+    The goal of the project is to make a database comparison.
+    In order to be able to make this maintainable we use pure CQRS pattern.
+    Supported databases:
+    - MySQL
+    """
+
+    print('Welcome in Database Comparator (Netguru RnD project) ✨')
+
+    if not is_valid_database_cli_arg(database):
+        sys.exit(f'ERROR! database argument is not valid - possible choices {SUPPORTED_DATABASES_CLI_ARGS}')
+
+    database_test_factory = MultipleDatabaseTypeFactory()
+    database_test = database_test_factory.create_database_test(database, operations_number)
+    database_test.present_results()
 
 
 if __name__ == '__main__':
