189257c4e3f500b407b6762d469fa233156f0050
Szymon Miks <szymon.miks@netguru.com>
2020-01-13 12:17:19 +0100

    [BN-1028]: Added unit tests for things related to the postgresql

--- src/tests/unit/test_factory.py
+++ src/tests/unit/test_factory.py
@@ -3,13 +3,21 @@ import pytest
 from src.core.exceptions import IllegalArgumentError
 from src.core.factory import MultipleDatabaseTypeFactory
 from src.performance.mysql.mysql_performance_test import MySQLPerformanceTest
+from src.performance.postgres.postgresql_performance_test import PostgreSQLPerformanceTest
 
 
-def test_multiple_database_type_factory_ok():
+def test_multiple_database_type_factory_mysql_ok():
     factory = MultipleDatabaseTypeFactory()
     db_test = factory.create_database_test('mysql', 10)
 
     assert isinstance(db_test, MySQLPerformanceTest)
+
+
+def test_multiple_database_type_factory_postgresql_ok():
+    factory = MultipleDatabaseTypeFactory()
+    db_test = factory.create_database_test('postgresql', 10)
+
+    assert isinstance(db_test, PostgreSQLPerformanceTest)
 
 
 def test_multiple_database_type_factory_with_bad_argument():
