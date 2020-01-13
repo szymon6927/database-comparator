8aa67f49e48aca00239df60245432547e3a72c55
Szymon Miks <szymon.miks@netguru.com>
2020-01-13 11:13:44 +0100

    [BN-1028]: Added performance test for postgresql

---
+++ src/performance/postgres/postgresql_performance_test.py
@@ -0,0 +1,41 @@
+from src.drivers.postgres.impls.postgres_repositories import CustomerPostgresRepository
+from src.drivers.postgres.impls.postgres_repositories import EventPostgresRepository
+from src.drivers.postgres.impls.postgres_repositories import OrderPostgresRepository
+from src.performance.performance_test import BasePerformanceTest
+
+
+class PostgreSQLPerformanceTest(BasePerformanceTest):
+    def __init__(self, operations_number: int):
+        super().__init__(
+            operations_number, CustomerPostgresRepository(), EventPostgresRepository(), OrderPostgresRepository()
+        )
+
+    def present_results(self) -> None:
+        customers_table_results = self.test_customers_table()
+        events_table_results = self.test_events_table()
+        orders_table_results = self.test_orders_table()
+
+        print('> PERFORMANCE TESTS RESULTS')
+        print('> DATABASE TYPE: PostgreSQL')
+        print(f'> Operations number: {self.operations_number}')
+
+        print('')
+
+        print(f'>> For {customers_table_results.table_name} table:')
+        print(f'>>> Inserting {self.operations_number} customers: {customers_table_results.insert_time}s')
+        print(f'>>> Get by id {self.operations_number} customers: {customers_table_results.get_by_id_time}s')
+        print(f'>>> Get all {self.operations_number} customers: {customers_table_results.get_all_time}s')
+
+        print('')
+
+        print(f'>> For {events_table_results.table_name} table:')
+        print(f'>>> Inserting {self.operations_number} events: {events_table_results.insert_time}s')
+        print(f'>>> Get by id {self.operations_number} events: {events_table_results.get_by_id_time}s')
+        print(f'>>> Get all {self.operations_number} events: {events_table_results.get_all_time}s')
+
+        print('')
+
+        print(f'>> For {orders_table_results.table_name} table:')
+        print(f'>>> Inserting {self.operations_number} orders: {orders_table_results.insert_time}s')
+        print(f'>>> Get by id {self.operations_number} orders: {orders_table_results.get_by_id_time}s')
+        print(f'>>> Get all {self.operations_number} orders: {orders_table_results.get_all_time}s')
